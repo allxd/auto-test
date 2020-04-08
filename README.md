c63611d9942ebb1ded23658f7833fad9a626f2b6
Александр Дудников <a.dudnikov@cinet.ru>
2020-04-08 10:33:21 +0300

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+Как развернуть проект:
+1. composer install
+
+Chromedriver:
+1. https://chromedriver.chromium.org/downloads
+2. chromedriver --url-base=wd/hub/
+
+Чтобы запустить тесты
+1. php vendor/codeception/codeception/codecept run
---
+++ tests/acceptance/LoginCest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests\acceptance;
+
+use App\Tests\AcceptanceTester;
+use function PHPUnit\Framework\assertGreaterThanOrEqual;
+
+class LoginCest
+{
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function loginFormFillTest(AcceptanceTester $I)
+    {
+        $I->wantToTest('Логин и выписка');
+        $I->retry(4, 400);
+        $I->amOnPage('/');
+        $I->seeElement('img[alt="Интернет банк - Банк Санкт-Петербург"]');
+        $I->seeElement('input[name="username"]');
+        $I->seeElement('input[name="password"]');
+        $I->seeElement('#login-button');
+        $I->click('#login-button');
+        $I->waitForElement('#otp-code', 30);
+        $I->seeElement('#login-otp-button');
+        $I->click('#login-otp-button');
+        $I->waitForElement('#accounts-index', 30);
+        $I->moveMouseOver( '#accounts-index');
+        $I->retrySee('Выписка');
+        $I->click('#statement');
+        $I->waitForElement('#query-button', 30);
+        $I->click('#query-button');
+        $rows = $I->grabMultiple('.transaction-row');
+        assertGreaterThanOrEqual(2, count($rows));
+    }
+}
